350da785635cbcf58603884f835533f97a64c16a
DS <ds312306@gmail.com>
2022-03-22 00:52:58 +0800

    add Tailwind css styles for index page

---
+++ pages/orders.tsx
@@ -0,0 +1,25 @@
+
+
+import { ReactElement } from "react";
+import Order_Layout from "@layout/Order_Layout";
+import { PageTitle } from './products/index.style'
+
+
+const Orders = () => {
+  
+  return <PageTitle> 目前位置 : 訂單 SSSS </PageTitle>
+  
+} ;
+
+export default Orders
+       
+
+// Orders.Layout = Layout;
+
+Orders.getLayout = function getLayout( page : ReactElement ){
+
+
+  return <Order_Layout> { page }  </Order_Layout>
+
+
+}
